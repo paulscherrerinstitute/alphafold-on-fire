0a378d715c01e590cb3ffe59f75a871093f11a0e
Krisztian Pozsa <pozsa@users.noreply.github.com>
2023-05-08 11:35:36 +0200

    Configure Ruff as a linter for the backend
    https://beta.ruff.rs/docs/

--- backend/app/schemas/health.py
+++ backend/app/schemas/health.py
@@ -13,5 +13,5 @@ class Status(str, enum.Enum):
 class Health(pydantic.BaseModel):
     status: Status
     version: str
-    releaseId: str
+    releaseId: str  # noqa: N815
     checks: dict[str, list[dict[str, Any]]]
