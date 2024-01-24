2bd191bb0d8a021ea844ac9e5d9dbb124a663810
Krisztian Pozsa <pozsa@users.noreply.github.com>
2024-01-24 11:18:42 +0100

    Add auth.js integration for easier oidc auth
    More about auth.js:
    https://authjs.dev/getting-started/providers/oauth-tutorial?frameworks=sveltekit

--- frontend/tests/test.ts
+++ frontend/tests/test.ts
@@ -4,3 +4,10 @@ test('index page has expected h1', async ({ page }) => {
 	await page.goto('/');
 	await expect(page.getByRole('heading', { name: 'AlphaFold-on-Fire' })).toBeVisible();
 });
+
+test('signin page has edu-ID button', async ({ page }) => {
+	await page.goto('/auth/signin');
+	await expect(
+		page.getByRole('button', { name: 'Sign in with SWITCH edu-ID' })
+	).toBeVisible();
+});
